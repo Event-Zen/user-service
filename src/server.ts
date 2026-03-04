21445f8de578c54768dcc71de0560a35293a55f3
bhashana99 <bhashanachamodya99@gmail.com>
2026-03-04 08:40:10 +0530

    connect to mongo db cluster

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import "dotenv/config";
 import { createApp } from "./app";
+import { connectDB } from "./config/db";
 
 const port = Number(process.env.PORT || 4001);
 
-createApp().listen(port, () => {
-  console.log(`user-service running on http://localhost:${port}`);
+async function start() {
+  await connectDB();
+  createApp().listen(port, () => console.log(`user-service on ${port}`));
+}
+
+start().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
